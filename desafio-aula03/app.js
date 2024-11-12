55c26b904d7aee5418373abf4d991ef98945d6f6
Guilherme Alves Carchedi <guilhermealves.carchedi@gmail.com>
2024-11-12 14:27:16 -0300

    desafio 03 do curso de JavaScript do curso da alura

---
+++ desafio-aula03/app.js
@@ -0,0 +1,29 @@
+// Atividade 1
+let contador = 1;
+while(contador <= 10){
+    alert(contador);
+    contador++;
+}
+
+// Atividade 2
+let contador2 = 10;
+while(contador2 >= 0){
+    alert(contador2);
+    contador2--;
+}
+
+// Atividade 3
+let numeroMax = prompt('Digite um numero para a contagem regressiva:');
+
+while(numeroMax >= 0){
+    alert(numeroMax);
+    numeroMax--;
+}
+
+// Atividade 4
+let numeroMin = prompt('Digite um número para a contagem progressiva:');
+
+while(numeroMin <= 10){
+    alert(numeroMin);
+    numeroMin++;
+}
